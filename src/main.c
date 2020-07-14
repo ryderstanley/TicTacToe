a2d036624877b0c8da8a496bf445cdd2627156a9
ryderstanley <ryderstanley2007@gmail.com>
2020-07-14 16:38:31 -0600

    started

---
+++ src/main.c
@@ -0,0 +1,14 @@
+// our imports
+#include <stdio.h>
+
+// starts our main class
+int main() {
+  
+  // creates three characters for where to move
+  char[] a1 = "a1";
+  char[] a2 = "a2";
+  char[] a3 =;
+
+  switch(mov1) {
+
+    case
